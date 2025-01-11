056128c94311f745d44745ae5b2813ae523d1093
Aashis Rijal <aashisrijal252@gmail.com>
2025-01-11 17:43:39 +0545

    product apis

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,10 +4,16 @@ const router = express.Router();
 const { isAuthenticated } = require('../middleware/isAuthenticated');
 const { allowedTo } = require('../middleware/allowedTo');
 const { upload } = require('../middleware/multerConfig');
-const { addProduct, getAllProduct } = require('../controllers/productControler');
+const { addProduct, getAllProduct, getSingleProduct, deleteProduct, updateProduct } = require('../controllers/productController');
 const { errorHandler } = require('../services/catchAsyncError');
 
-router.route("/product").post(isAuthenticated,allowedTo('admin'),upload.single('image'),addProduct).get(errorHandler(getAllProduct))
-router.route("/product/:id").patch().delete();
+router.route("/product")
+.post(isAuthenticated,allowedTo('admin'),upload.single('image'),errorHandler(addProduct))
+.get(errorHandler(getAllProduct))
+
+router.route("/product/:id")
+.get(errorHandler(getSingleProduct))
+.patch(isAuthenticated,allowedTo('admin'),upload.single('image'),errorHandler(updateProduct))
+.delete(isAuthenticated,allowedTo('admin'),errorHandler(deleteProduct));
 
 module.exports = router;
