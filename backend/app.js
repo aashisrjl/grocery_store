27313415195e35d86f540ddc2a019676e4ee5852
Aashis Rijal <aashisrijal252@gmail.com>
2025-01-12 12:13:59 +0545

    initialize oauth

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,15 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
 
 
+app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 //database connection
 require('./models/index.js');
@@ -27,10 +32,30 @@ categorySeeder()
 const authRoute = require("./routes/authRoute.js");
 const categoryRoute = require("./routes/categoryRoute.js");
 const productRoute = require("./routes/productRoute.js");
+const { users } = require("./models/index.js");
+
+// serialize and deserialize user
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+passport.deserializeUser((id, cb) => {
+        cb(null, id);
+    });
 
 app.use("/",authRoute)
 app.use("/",categoryRoute)
 app.use("/",productRoute)
+
+//google authentication google strategy
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+    passport.use(new googleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
+    }, async function(accessToken, refreshToken, profile, done) {
+        
+    }))
+
 
 
 app.listen(port, () => {
