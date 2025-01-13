55ef0bc7fc2ffa61408ebb9b1168bc076cbb2de7
Aashis Rijal <aashisrijal252@gmail.com>
2025-01-13 20:36:09 +0545

    delete and update cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,5 +1,6 @@
 const { product, users, cart } = require("../models")
 
+
 //add to cart by user
 exports.addToCart = async(req,res)=>{
     const userId = req?.userId
@@ -51,7 +52,12 @@ exports.getCartItem = async(req,res)=>{
         {
             model:users,
             as:"userDetails"
-        }]
+        },
+    {
+        model: category,
+        as:"categoryDetails",
+        attributes:["categoryName"]
+    }]
     })
     res.status(200).json({
         data:cartItem
@@ -62,4 +68,58 @@ exports.getCartItem = async(req,res)=>{
             message:"No items in the cart for this user"
         })
     }
+}
+
+//delete cart item
+exports.deleteCart = async(req,res)=>{
+    const userId = req?.userId
+    const productId = req.params.productId
+    const product = await product.findByPk(productId);
+    if(!product){
+        return res.status(400).json({
+            message:"No product found"
+        })
+    }
+    await cart.destroy({
+        where:{
+            userId,
+            productId
+        }
+    })
+    res.status(200).json({
+        message:"Product removed from cart"
+    })  
+}
+
+//update a cart item
+
+exports.updateCart = async(req,res)=>{
+    const userId = req?.userId
+    const productId = req.params.productId
+    const {quantity} = req.body
+    const product = await product.findByPk(productId);
+
+    if(!product){
+        return res.status(400).json({
+            message:"No product found"
+        })
+    }
+    const cartItem = await cart.findOne({
+        where:{
+            userId,
+            productId
+        }
+    })
+
+    if(!cartItem){
+        return res.status(400).json({
+            message:"No product found in the cart"
+        })
+    }
+    cartItem.quantity = quantity
+    await cartItem?.save();
+    res.status(200).json({
+        message:"Cart updated",
+        data:cartItem
+    })
 }
